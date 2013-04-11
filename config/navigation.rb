c7b95a2b849bd331b92c91ab50bbeac964146115
David Chawei Hsu <chawei.hsu@gmail.com>
2013-04-11 10:36:00 +0800

    working on menu

--- config/navigation.rb
+++ config/navigation.rb
@@ -7,11 +7,11 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.item :films, 'Films', admin_films_path, :highlights_on => /admin\/films/, :if => Proc.new { in_admin? }
     primary.item :pages, 'Pages', admin_pages_path, :highlights_on => /admin\/pages/, :if => Proc.new { in_admin? }
     
-    primary.item :articles, "<span class='hidden'>News</span>".html_safe, articles_path, :if => Proc.new { !in_admin? }
-    primary.item :films, "<span class='hidden'>Films</span>".html_safe, films_path, :highlights_on => /\/films/, :if => Proc.new { !in_admin? }
-    primary.item :albums, "<span class='hidden'>Photography</span>".html_safe, albums_path, :highlights_on => /\/albums/, :if => Proc.new { !in_admin? }
-    primary.item :bio, "<span class='hidden'>Bio</span>".html_safe, page_path('bio'), :if => Proc.new { !in_admin? }
-    primary.item :cv, "<span class='hidden'>C.V.</span>".html_safe, page_path('cv'), :if => Proc.new { !in_admin? }
-    primary.item :contact, "<span class='hidden'>Contact</span>".html_safe, contact_path, :if => Proc.new { !in_admin? }
+    primary.item :articles, "WORK".html_safe, articles_path, :if => Proc.new { !in_admin? }
+    #primary.item :films, "<span class='hidden'>Films</span>".html_safe, films_path, :highlights_on => /\/films/, :if => Proc.new { !in_admin? }
+    #primary.item :albums, "<span class='hidden'>Photography</span>".html_safe, albums_path, :highlights_on => /\/albums/, :if => Proc.new { !in_admin? }
+    primary.item :bio, "BIOGRAPHY".html_safe, page_path('bio'), :if => Proc.new { !in_admin? }
+    primary.item :cv, "REVIEW".html_safe, page_path('cv'), :if => Proc.new { !in_admin? }
+    primary.item :contact, "NEWS / CONTACT".html_safe, contact_path, :if => Proc.new { !in_admin? }
   end
 end
