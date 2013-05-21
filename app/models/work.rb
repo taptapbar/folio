3a1be4497ce1e00cd4d350832a8430b1a5e7c574
David Chawei Hsu <chawei.hsu@gmail.com>
2013-05-21 16:50:38 -0700

    make Work sort by date and increase year range

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,11 +1,12 @@
 class Work < ActiveRecord::Base
   include ValidatesAsImage
   
+  default_scope :order => "date DESC"
+  
   attr_accessible :title, :date, :description, :image
 
   validates_as_image :image
-  has_attached_file  :image, :styles => { large: "990",
-  										  thumb: "138" }
+  has_attached_file  :image, :styles => { large: "990", thumb: "138" }
 
   validates_attachment_presence :image
   #validates_attachment_size :image, :less_than => 5.megabytes, :if => Proc.new { |imports| !imports.data.file? }
